4a7fd84e2ab8b8d17b67897280fda31ec307c4d6
Shahroze Abbas <28969387+shahrozeabbas@users.noreply.github.com>
2023-06-22 10:00:53 -0400

    Add files via upload

--- snakemake.sh
+++ snakemake.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH --mem=400g
-#SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=20
+#SBATCH --mem=800g
+#SBATCH --time=6:00:00
+#SBATCH --cpus-per-task=36
 #SBATCH --partition=largemem
 #SBATCH --mail-type=ALL,TIME_LIMIT_50
 
-source /data/abbass2/Apps/conda/bin/activate snakes
+module load snakemake
 
-snakemake --use-conda --cores 16
+snakemake --use-conda --cores 36
